9ec61c80402ff47c35f4170349973f588d1bec4a
Sena Cahya <senacandrampc@gmail.com>
2021-02-12 21:38:21 +0800

    Add project files.

---
+++ RamenFixer/Plugin.cs
@@ -0,0 +1,66 @@
+﻿using IPA;
+using IPA.Config;
+using IPA.Config.Stores;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using IPALogger = IPA.Logging.Logger;
+using BS_Utils;
+
+namespace RamenFixer
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+
+        [Init]
+        public void Init(IPALogger logger)
+        {
+            Instance = this;
+            Log = logger;
+            Log.Info("RamenFixer initialized.");
+        }
+
+        #region BSIPA Config
+        //Uncomment to use BSIPA's config
+        /*
+        [Init]
+        public void InitWithConfig(Config conf)
+        {
+            Configuration.PluginConfig.Instance = conf.Generated<Configuration.PluginConfig>();
+            Log.Debug("Config loaded");
+        }
+        */
+        #endregion
+
+        [OnStart]
+        public void OnApplicationStart()
+        {
+            Log.Debug("OnApplicationStart");
+
+            BS_Utils.Utilities.BSEvents.gameSceneLoaded += GameSceneLoaded;
+            BS_Utils.Utilities.BSEvents.menuSceneLoaded += MenuSceneLoaded;
+        }
+
+        private void GameSceneLoaded()
+        {
+            RamenFixerController.LoadRamenFixer();
+        }
+
+        private void MenuSceneLoaded()
+        {
+            RamenFixerController.UnloadRamenFixer();
+        }
+
+        [OnExit]
+        public void OnApplicationQuit()
+        {
+            Log.Debug("OnApplicationQuit");
+        }
+    }
+}
---
+++ RamenFixer/RamenFixerController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace RamenFixer
+{
+    public class RamenFixerController : MonoBehaviour
+    {
+        private GameObject _playerGameObject;
+        private GameObject _vmcAvatarGameObject;
+        private Vector3 _originAvatarPosition;
+        private Quaternion _originAvatarRotation;
+        public static RamenFixerController Instance { get; private set; }
+        
+        private void Awake()
+        {
+            _playerGameObject = GameObject.Find("LocalPlayerGameCore/Origin");
+            _vmcAvatarGameObject = GameObject.Find("VRM");
+            _originAvatarPosition = _vmcAvatarGameObject.transform.localPosition;
+            _originAvatarRotation = _vmcAvatarGameObject.transform.localRotation;
+        }
+
+        internal static void LoadRamenFixer()
+        {
+            if (Instance != null)
+            {
+                Destroy(Instance.gameObject);
+            }
+            GameObject ramen = new GameObject("Ramen");
+            Instance = ramen.AddComponent<RamenFixerController>();
+        }
+
+        internal static void UnloadRamenFixer()
+        {
+            if (Instance != null)
+            {
+                Instance.ResetAvatarPosition();
+                Destroy(Instance.gameObject);
+                Instance = null;
+            }
+        }
+
+        void ResetAvatarPosition()
+        {
+            if(_vmcAvatarGameObject != null)
+            {
+                _vmcAvatarGameObject.transform.localPosition = _originAvatarPosition;
+                _vmcAvatarGameObject.transform.localRotation = _originAvatarRotation;
+            }
+        }
+
+        private void Update()
+        {
+            
+            if(_playerGameObject != null && _vmcAvatarGameObject != null)
+            {
+                _vmcAvatarGameObject.transform.localPosition = _playerGameObject.transform.localPosition;
+                _vmcAvatarGameObject.transform.localRotation = _playerGameObject.transform.localRotation;
+            }
+        }
+    }
+}
